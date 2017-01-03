b0067b9b72c2b9789be8512b95840825db1970bc
zzh <775665615@qq.com>
2017-01-03 14:30:22 +0800

    modified:   cfg/defaults.js  	new file:   dist/assets/1df9ea530ae53824c9b274f835a8ddaf.jpg  	new file:   dist/assets/36eb3928e9378050196ab6ca6bdc3259.jpg  	new file:   dist/assets/3894df5e0badb02b898a80c1dba235a2.jpg  	new file:   dist/assets/5484091ce949b141509f51fc67eb3f03.jpg  	new file:   dist/assets/600a51429f8031d1b7556bbbeff77628.jpg  	new file:   dist/assets/69586e5b1890f6e243cd23a8ca6b7100.jpg  	new file:   dist/assets/840868d85b4e46fa6694d53687703f58.jpg  	new file:   dist/assets/a4c72f121de92f85a73f9f3c380c41ca.jpg  	modified:   dist/assets/app.js  	new file:   dist/assets/bc5eb83940bb5ee7948f82585bffdc72.jpg  	new file:   dist/assets/d011fd28541a30f82f67562e50561e18.jpg  	new file:   dist/assets/f3de933033637901686f37f96d785896.jpg  	modified:   dist/index.html  	modified:   src/components/Main.js  	modified:   src/index.html

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ require('styles/App.scss');
 import React from 'react';
 // 获取图片相关的数据
 var imageDatas = require('../data/imageDatas.json');
-
+let yeomanImage = require('../images/yeoman.png');
 // 利用自执行函数，将图片名信息转成图片URL路径信息
 imageDatas=(function getImageURL(imageDatasArr){
     for(var i=0,j=imageDatasArr.length;i<j;i++){
@@ -19,16 +19,16 @@ imageDatas=(function getImageURL(imageDatasArr){
 class AppComponent extends React.Component {
   render() {
     return (
-    <div className="index">
-      <img src={yeomanImage} alt="Yeoman Generator" />
-      <div className="notice">Please hhh <code>src/components/Main.js</code> to get started!</div>
-    </div>
-      // <section className="stage">
-      //     <section className="img-sec">
-      //     </section>
-      //     <nav className="controller-nav">
-      //     </nav>
-      // </section>
+    // <div className="index">
+    //   <img src={yeomanImage} alt="Yeoman Generator" />
+    //   <div className="notice">Please hhh <code>src/components/Main.js</code> to get started!</div>
+    // </div>
+      <section className="stage">
+          <section className="img-sec">
+          </section>
+          <nav className="controller-nav">
+          </nav>
+      </section>
     );
   }
 }
